bca2be3b3a60a2573581643c80d9727c35c0a99a
Sam Schiff <samuel.b.schiff.20@dartmouth.edu>
2019-06-14 23:56:41 -0400

    added router example

--- src/server.js
+++ src/server.js
@@ -1,9 +1,12 @@
 import express from 'express';
+import api from './api';
 
 const app = express();
 
-app.get('/', (req,res) => {
-    res.status(200).json({message: "express server with es6"});
+app.get('/', (req, res) => {
+  res.status(200).json({ message: 'express server with es6' });
 });
 
-app.listen(process.env.PORT || 3000, () => console.log("Listening to port 3000"));
+app.use('/api', api);
+
+app.listen(process.env.PORT || 3000, () => console.log('Listening to port 3000'));
